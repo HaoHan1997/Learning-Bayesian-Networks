5bc910e920e66cb387be26a06efa108d04b27268
HaoHan1997 <2784317596@qq.com>
2020-05-04 22:26:45 +0800

    modify DP test.m

--- DynamicProgramming/MATLAB/test.m
+++ DynamicProgramming/MATLAB/test.m
@@ -1,3 +1,8 @@
 clc;
 clear;
 bent = mk_asia();
+ori_dag = bnet.dag;
+figure(1);draw_graph(ori_dag);title('original net');
+N = length(ori_dag);% the number of nodes
+ns = bnet.node_sizes;
+nsamples = 1000;
